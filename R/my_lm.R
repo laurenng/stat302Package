e3be0e3d69576476bf6399d4b7a328e704f1bd0d
Lauren Ng <2014laurenng@gmail.com>
2021-03-17 16:30:53 -0700

    resolved check errors

--- R/my_lm.R
+++ R/my_lm.R
@@ -5,7 +5,7 @@
 #'
 #' @param formula formula for the linear model (ie: lifeExp ~ gdpPercap + pop)
 #' @param data data frame the formula is based on
-#' @keywords linear model, prediction
+#' @keywords linear model, prediction, inference
 #'
 #' @return table with rows for each coefficient and columns for the Estimate,
 #'         Std. Error, t value, and Pr(>|t|)
